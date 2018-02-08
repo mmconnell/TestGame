f81e137286281b544088c36bfb9f9e7696fb5f78
Michael McConnell <mmcconnell@commerce-architects.com>
2018-02-07 16:38:32 -0800

    New stuff

---
+++ StatusEffect/DerivedStatusEffect/Extended/AuraOfBlind.cs
@@ -0,0 +1,7 @@
+public class AuraOfBlind : DerivedStatusEffect 
+{
+    public AuraOfBlind() 
+    {
+       // AddBaseStatusEffect(new AuraStatusEffect(new EffectPack(50, )))
+    }
+}
---
+++ StatusEffect/DerivedStatusEffect/Extended/Blind.cs
@@ -0,0 +1,7 @@
+public class Blind : DerivedStatusEffect
+{
+    public Blind() 
+    {
+        
+    }
+}
